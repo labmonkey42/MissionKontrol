50f51aaa6010c264573592b94a78f1951af245f7
Chris Lee <labmonkey42@gmail.com>
2025-10-20 11:59:10 +0000

    Install Tauri dependencies  in postCreateCommand script

--- .devcontainer/app/postCreateCommand.sh
+++ .devcontainer/app/postCreateCommand.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 
 echo "Running post-create commands..."
+
+echo "Installing npm packages..."
 npm install
+echo "Done installing npm packages."
+
+echo "Installing Tauri dependencies..."
+sudo apt update
+sudo apt install \
+  libwebkit2gtk-4.1-dev \
+  build-essential \
+  curl \
+  wget \
+  file \
+  libxdo-dev \
+  libssl-dev \
+  libayatana-appindicator3-dev \
+  librsvg2-dev
+echo "Done installing Tauri dependencies."
