f31cb70cc01dda54052611971d6a90c5040bf359
Chris Lee <chrismlee42@gmail.com>
2026-03-26 18:05:26 +0000

    WIP

--- .devcontainer/app/postCreateCommand.sh
+++ .devcontainer/app/postCreateCommand.sh
@@ -6,16 +6,16 @@ echo "Installing npm packages..."
 npm install
 echo "Done installing npm packages."
 
-echo "Installing Tauri dependencies..."
-sudo apt update
-sudo apt install -y \
-  libwebkit2gtk-4.1-dev \
-  build-essential \
-  curl \
-  wget \
-  file \
-  libxdo-dev \
-  libssl-dev \
-  libayatana-appindicator3-dev \
-  librsvg2-dev
-echo "Done installing Tauri dependencies."
+# echo "Installing Tauri dependencies..."
+# sudo apt update
+# sudo apt install -y \
+#   libwebkit2gtk-4.1-dev \
+#   build-essential \
+#   curl \
+#   wget \
+#   file \
+#   libxdo-dev \
+#   libssl-dev \
+#   libayatana-appindicator3-dev \
+#   librsvg2-dev
+# echo "Done installing Tauri dependencies."
